16047e8581912c9ea8116a541c41804d6bb0cfee
jerge <erikjer@student.chalmers.se>
2020-02-06 13:12:38 +0100

    p3,2:50

---
+++ KA-Flocking/Assets/Flock.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+
+    public FlockAgent agentPrefab;
+    const float agentDensity = 0.8f;
+    List<FlockAgent> agents = new List<FlockAgent>();
+    public FlockBehaviour behaviour;
+
+    [Range(10,100)]
+    public int startingAmount = 50;
+    [Range(1f,100f)]
+    public float driveFactor = 10f;
+    [Range(1f,100f)]
+    public float maxSpeed = 5f;
+    [Range(1f,10f)]
+    public float neighbourRadius = 1.5f;
+    [Range(0f,1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float squareMaxSpeed,squareNeighbourRadius,squareAvoidanceRadius;
+    public float SquareAvoidanceRadius {get {return squareAvoidanceRadius;}}
+    // Start is called before the first frame update
+    void Start()
+    {
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighbourRadius = neighbourRadius * neighbourRadius;
+        squareAvoidanceRadius = avoidanceRadiusMultiplier * avoidanceRadiusMultiplier * squareNeighbourRadius;
+
+        for (int i = 0; i < startingAmount; i++)
+        {
+            FlockAgent newAgent = Instantiate (
+                agentPrefab,
+                Random.insideUnitCircle * startingAmount * agentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f,360f)),
+                transform
+            );
+            newAgent.name = "Agent " + i;
+            agents.Add(newAgent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (FlockAgent agent in agents)
+        {
+            List<Transform> context = GetNearbyObjects(agent);
+
+        }
+    }
+
+    List<Transform> GetNearbyObjects(FlockAgent agent) {
+        List<Transform> context = new List<Transform>();
+        //Collider[] contextCollider = Physics3D.OverlapCircleAll
+    }
+}
