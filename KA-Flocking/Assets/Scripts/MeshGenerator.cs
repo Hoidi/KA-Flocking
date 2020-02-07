f7ae13e401299ec1b3fb333d11d48c77c25fbafd
Max <maxarfvidsson@gmail.com>
2020-02-07 10:44:26 +0100

    Genererar plan med kollisioner.

---
+++ KA-Flocking/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+        updateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int z = 0, i = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++) 
+        { 
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[0 + tris] = vert + 0;
+                triangles[1 + tris] = vert + xSize + 1;
+                triangles[2 + tris] = vert + 1;
+                triangles[3 + tris] = vert + 1;
+                triangles[4 + tris] = vert + xSize + 1;
+                triangles[5 + tris] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }  
+    }
+
+
+    void updateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+}
