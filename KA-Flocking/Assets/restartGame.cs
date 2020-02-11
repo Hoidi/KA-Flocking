8e89ebfcd8d2236deddbedb2d4e078ccc7c1d9a7
DecibelM <42909365+DecibelM@users.noreply.github.com>
2020-02-11 15:20:50 +0100

    Created first version of ending scene

---
+++ KA-Flocking/Assets/restartGame.cs
@@ -0,0 +1,23 @@
+﻿//This script is controlling the button.
+//Later on this script will fetch the scene that starts a new game.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class restartGame : MonoBehaviour
+{
+    public Text text;
+	public Button yourButton;
+
+		void Start()
+		{
+			//Button btn = yourButton.GetComponent<Button>();
+			yourButton.onClick.AddListener(ResetGame);
+		}
+
+		void ResetGame()
+		{
+		text.text = ("Yaaay!");
+		}
+}
+
