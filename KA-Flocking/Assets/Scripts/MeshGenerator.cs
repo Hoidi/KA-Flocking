48dab71ee5b372488099b7f39770210d90bd18ad
Max <maxarfvidsson@gmail.com>
2020-02-11 13:44:14 +0100

    adds adjusters and enables octaves

--- KA-Flocking/Assets/Scripts/MeshGenerator.cs
+++ KA-Flocking/Assets/Scripts/MeshGenerator.cs
@@ -11,8 +11,15 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int resolution = 1;
     public int xSize = 20;
     public int zSize = 20;
+    public int octaves = 1;
+    public float height = 0.3f;
+    public float scale = 0.3f;
+    [Range(0, 1)]
+    public float percistance = 0.5f;
+    public float lacunarity = 5f;
     public 
 
     // Start is called before the first frame update
@@ -26,14 +33,37 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape()
     {
+        xSize *= resolution;
+        zSize *= resolution;
+        scale /= resolution;
+
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        Debug.Log((xSize + 1) * (zSize + 1));
 
         for (int z = 0, i = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
-                vertices[i] = new Vector3(x, y, z);
+                if (lacunarity < 1)
+                {
+                    lacunarity = 1;
+                }
+                float amplitude = 1f;
+                float frequency = 1f;
+                float noiceHeight = 0f;
+                float y = 0;
+                for(int j = 0; j< octaves; j++)
+                {
+                    float sampleX = x * frequency * scale;
+                    float sampleZ = z * frequency * scale;
+
+                    y = Mathf.PerlinNoise(sampleX, sampleZ) * 2f -1f;
+                    noiceHeight += y * amplitude;
+
+                    amplitude *= percistance;
+                    frequency *= lacunarity;
+                }
+                vertices[i] = new Vector3(x / (float)resolution, y * height, z / (float)resolution);
                 i++;
             }
 
