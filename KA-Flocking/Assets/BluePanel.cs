acbac0efcafb9e85a6fcec929dba0f6b5c2d1e2d
DecibelM <42909365+DecibelM@users.noreply.github.com>
2020-02-16 17:05:52 +0100

    Score panel design - font and displaying number

---
+++ KA-Flocking/Assets/BluePanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BluePanel : MonoBehaviour
+{
+    public Text infantryNoText;
+    public Flock blueFlock;
+    private int infantryNo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        infantryNoText.text = blueFlock.agents.Count.ToString();
+        Debug.Log(blueFlock.agents.Count.ToString());
+    }
+}
