00fc9623fd7843db472c53b1cd9d7077cfe62fde
Sigbear <gusdrssi@student.gu.se>
2020-02-18 10:00:59 +0100

    filters added for behavior
    Behaviors can now filter what they react on.

--- KA-Flocking/Assets/BehaviourScripts/AvoidanceBehaviour.cs
+++ KA-Flocking/Assets/BehaviourScripts/AvoidanceBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Flock/Behaviour/Avoidance")]
-public class AvoidanceBehaviour : FlockBehaviour
+public class AvoidanceBehaviour : FilteredFlockBehaviour
 {
     public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
         if (context.Count == 0)
@@ -14,7 +14,8 @@ public class AvoidanceBehaviour : FlockBehaviour
         Vector3 avoidanceMove = Vector3.zero;
         int avoidAmount = 0;
         float distanceSqr;
-        foreach (Transform item in context)
+        List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+        foreach (Transform item in filteredContext)
         {
             distanceSqr = Vector3.SqrMagnitude(item.position - agent.transform.position);
             if (distanceSqr < flock.SquareAvoidanceRadius) {
