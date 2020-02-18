07951dc6c1338501af39a14c491e846b433ce72d
Simonolsson98 <Simol@student.chalmers.se>
2020-02-18 13:21:41 +0100

    can now choose how many troops to spawn, rectangular formation implemented, triangular form *TBD*

--- KA-Flocking/Assets/Scripts/EntitySpawning.cs
+++ KA-Flocking/Assets/Scripts/EntitySpawning.cs
@@ -15,9 +15,14 @@ public class EntitySpawning : MonoBehaviour
     public Toggle infantryToggle;
     public Toggle pikeToggle;
     public Toggle archerToggle;
-    bool isButtonSelected = false;
-    IEnumerator<WaitForSeconds> newCoroutine(){
-        yield return new WaitForSeconds(1);
+    public Slider troopSlider;
+    private int amountOfTroops;
+    public Text troopText;
+    public void setTroopAmount()
+    {
+        amountOfTroops = (int) troopSlider.value;
+        troopText.text = amountOfTroops.ToString();
+
     }
     public void spawnOne(){
         if(infantryToggle.isOn){
@@ -70,18 +75,56 @@ public class EntitySpawning : MonoBehaviour
 
             FlockAgent spawnedAgent = Instantiate(troop, worldPos, transform.rotation); //spawn the troop at cursor location (=+ 1 for y coord)
             flock.agents.Add(spawnedAgent); //add the newly spawned troop into the list of flock members
-            StartCoroutine(newCoroutine());
-        
-        }
+            }
     }
 
     public void spawnFiveEntites(FlockAgent troop){
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        
+        if (Input.GetKey("space")){ //shortcut to place units, prone to change(?)
+            Vector3 worldPos;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition); //raytracing towards mouse cursor from camera
+            if (Physics.Raycast(ray, out collisionWithPlane, 100f))
+            { //raytracing collided with something
+                worldPos = collisionWithPlane.point; //convert pixel coordinates to normal coordinates
+                worldPos.y += 1f; //make sure they dont fall through the ground
+            }
+            else
+            { //no collision
+                worldPos = cam.ScreenToWorldPoint(mousePos);
+                worldPos.y += 1f; //make sure they dont fall through the ground
+            }
+            flock.agents.Add(Instantiate(troop, new Vector3(worldPos.x, worldPos.y, worldPos.z), transform.rotation));
+            for (int i = 1; i < amountOfTroops; i++){
+                flock.agents.Add(Instantiate(troop, new Vector3(worldPos.x + 5 * (i % 5), worldPos.y, worldPos.z + 5 * Mathf.CeilToInt(i / 5)), transform.rotation)); //spawn troops in formation
 
+            }
 
+        }
     }
 
     public void spawnTriangleShapedEntites(FlockAgent troop){
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
-        
+        if (Input.GetKey("space"))
+        { //shortcut to place units, prone to change(?)
+            Vector3 worldPos;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition); //raytracing towards mouse cursor from camera
+            if (Physics.Raycast(ray, out collisionWithPlane, 100f))
+            { //raytracing collided with something
+                worldPos = collisionWithPlane.point; //convert pixel coordinates to normal coordinates
+                worldPos.y += 1f; //make sure they dont fall through the ground
+            }
+            else
+            { //no collision
+                worldPos = cam.ScreenToWorldPoint(mousePos);
+                worldPos.y += 1f; //make sure they dont fall through the ground
+            }
+            for (int i = 0; i < amountOfTroops; i++){
+                flock.agents.Add(Instantiate(troop, new Vector3(worldPos.x + 5 * (i % 2), worldPos.y, worldPos.z - 5 * (i % 5)), transform.rotation)); //spawn troops in formation
+
+            }  
+        }
+
     }
 }
