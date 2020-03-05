3174a82489d33d217ad41ab70a58ae6c50b33526
Simonolsson98 <Simol@student.chalmers.se>
2020-03-05 13:29:35 +0100

    can no longer drag and drop troops in simulation scene!

--- KA-Flocking/Assets/Scripts/DragAndReposition.cs
+++ KA-Flocking/Assets/Scripts/DragAndReposition.cs
@@ -1,29 +1,22 @@
 ﻿
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 
 //credit to YoungjaeKim, https://forum.unity.com/threads/drag-drop-game-objects-without-rigidbody-with-the-mouse.64169/
-public class DragAndReposition : MonoBehaviour
-{
+public class DragAndReposition : MonoBehaviour{
     private bool _mouseState;
     int layerMask = 1 << 8;
     private GameObject target;
     public Vector3 screenSpace;
     public Vector3 offset;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) //mouse left click
-        {
-
+    void Update(){
+        //mouse left click, and currently in setup 1 or setup 2 scene
+        if (Input.GetMouseButtonDown(0) && (SceneManager.GetSceneByName("PlayerOneSetupScene").isLoaded || SceneManager.GetSceneByName("PlayerTwoSetupScene").isLoaded)){ 
+            
             RaycastHit hit;
             target = RetrieveTroopObject(out hit);
             if (target != null) //if raycast found a troop, change its position based on mouse cursor
@@ -33,12 +26,10 @@ public class DragAndReposition : MonoBehaviour
                 offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
             }
         }
-        if (Input.GetMouseButtonUp(0))
-        {
+        if (Input.GetMouseButtonUp(0)){
             _mouseState = false;
         }
-        if (_mouseState)
-        {
+        if (_mouseState){
             //keep track of the mouse position
             var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
 
@@ -51,8 +42,7 @@ public class DragAndReposition : MonoBehaviour
     }
 
 
-    GameObject RetrieveTroopObject(out RaycastHit hit)
-    {
+    GameObject RetrieveTroopObject(out RaycastHit hit){
         GameObject target = null;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //raytracing towards mouse cursor from camera
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
