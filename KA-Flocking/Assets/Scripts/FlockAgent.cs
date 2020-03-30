40c50d687ff7b8cb9831ba6058ba7c51c7236ce6
Max <maxarfvidsson@gmail.com>
2020-03-31 00:43:10 +0200

    fixed animations. Ez

--- KA-Flocking/Assets/Scripts/FlockAgent.cs
+++ KA-Flocking/Assets/Scripts/FlockAgent.cs
@@ -9,12 +9,13 @@ public class FlockAgent : MonoBehaviour
     public float stability = 0.3f;
     // The speed at which the rotation of the agents stabilises
     public float stabilisationSpeed = 2.0f;
+    public float speed = 20;
     public Unit unit;
     Collider agentCollider;
     Rigidbody rb;
-    int animationCounter = 100;
     public Collider AgentCollider { get { return agentCollider; } }
     public Animator animator;
+    int animationMode = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +33,7 @@ public class FlockAgent : MonoBehaviour
             return;
         }
 
-        rb.velocity += acceleration * Time.deltaTime * 7;
+        rb.velocity += acceleration * Time.deltaTime * 10;
 
         if (rb.velocity != Vector3.zero)
         {
@@ -41,13 +42,36 @@ public class FlockAgent : MonoBehaviour
             transform.forward = velocity;
         }
 
-        if (animationCounter < 100)
+        float sqrVelocity = rb.velocity.sqrMagnitude;
+        float[] sqrAnimationSpeeds = { 0, 1.5f, 3, 6, 9, 14, 25, 40, 60, 90,};
+        int newAnimationMode = animationMode;
+
+        //looks to see if the velocity has changed enough to enter a new range. 
+        //also ads a buffer of 0.2 and 0.3 respectilvy stopping a unity from jumping between two animations to frequently. 
+        for (int i = sqrAnimationSpeeds.Length-1; i >= 0; i--)
         {
-            animationCounter++;
+            if (animationMode > i && sqrAnimationSpeeds[i+1] * 0.8f > sqrVelocity)
+            {
+                newAnimationMode = i;
+                break;
+            }
+            else if (animationMode < i && sqrAnimationSpeeds[i] * 1.3f < sqrVelocity)
+            {
+                newAnimationMode = i;
+                break;
+            }
+        }
+
+        if(newAnimationMode == animationMode)
+        {
             stabiliseY();
             return;
         }
-        float sqrVelocity = rb.velocity.sqrMagnitude;
+        else
+        {
+            animationMode = newAnimationMode;
+        }
+
         animator.ResetTrigger("idle");
         animator.ResetTrigger("slowWalk");
         animator.ResetTrigger("walk");
@@ -58,52 +82,40 @@ public class FlockAgent : MonoBehaviour
         animator.ResetTrigger("slowNarutoRun");
         animator.ResetTrigger("narutoRun");
         animator.ResetTrigger("fastNarutoRun");
-        if (sqrVelocity >= 100)
-        {
-            animator.SetTrigger("fastNarutoRun");
-        }
-        else if (sqrVelocity >= 70)
-        {
-            animator.SetTrigger("narutoRun");
-        }
-        else if (sqrVelocity >= 48)
-        {
-            animator.SetTrigger("slowNarutoRun");
-        }
-        else if (sqrVelocity >= 25)
-        {
-            animator.SetTrigger("fastRun");
-            Debug.Log("fastRun");
-        }
-        else if (sqrVelocity >= 14)
-        {
-            animator.SetTrigger("run");
 
-            Debug.Log("run");
-        }
-        else if (sqrVelocity >= 9)
+        switch (animationMode)
         {
-            animator.SetTrigger("slowRun");
-
-            Debug.Log("slowRun");
+            case 0:
+                animator.SetTrigger("idle");
+                break;
+            case 1:
+                animator.SetTrigger("slowWalk");
+                break;
+            case 2:
+                animator.SetTrigger("walk");
+                break;
+            case 3:
+                animator.SetTrigger("fastWalk");
+                break;
+            case 4:
+                animator.SetTrigger("slowRun");
+                break;
+            case 5:
+                animator.SetTrigger("run");
+                break;
+            case 6:
+                animator.SetTrigger("fastRun");
+                break;
+            case 7:
+                animator.SetTrigger("slowNarutoRun");
+                break;
+            case 8:
+                animator.SetTrigger("narutoRun");
+                break;
+            case 9:
+                animator.SetTrigger("fastNarutorun");
+                break;
         }
-        else if (sqrVelocity >= 6)
-        {
-            animator.SetTrigger("fastWalk");
-        }
-        else if (sqrVelocity >= 3)
-        {
-            animator.SetTrigger("walk");
-        }
-        else if (sqrVelocity >= 1.5)
-        {
-            animator.SetTrigger("slowWalk");
-        }
-        else
-        {
-            animator.SetTrigger("idle");
-        }
-        animationCounter = 0;
         stabiliseY();
     }
 
