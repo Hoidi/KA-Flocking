f92aabad4d11bfd0d69c1eec1aa8faad1d1cc853
DecibelM <42909365+DecibelM@users.noreply.github.com>
2020-03-30 09:35:48 +0200

    Spawning panel for player one done

--- KA-Flocking/Assets/ToggleSwitch.cs
+++ KA-Flocking/Assets/ToggleSwitch.cs
@@ -15,6 +15,19 @@ public class ToggleSwitch : MonoBehaviour
     public GameObject ArrowToggleOn;
     public GameObject ArrowToggleOff;
 
+    public Toggle InfantryToggle;
+    public Toggle PikeToggle;
+    public Toggle ArcherToggle;
+    public Toggle ScoutToggle;
+    public GameObject InfantryToggleOn;
+    public GameObject InfantryToggleOff;
+    public GameObject PikeToggleOn;
+    public GameObject PikeToggleOff;
+    public GameObject ArcherToggleOn;
+    public GameObject ArcherToggleOff;
+    public GameObject ScoutToggleOn;
+    public GameObject ScoutToggleOff;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +38,7 @@ public class ToggleSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Toggle buttons for formation type
         if (CircularToggle.isOn)
         {
             CircularToggleOn.SetActive(true);
@@ -57,5 +71,51 @@ public class ToggleSwitch : MonoBehaviour
             ArrowToggleOn.SetActive(false);
             ArrowToggleOff.SetActive(true);
         }
+        //Toggle buttons for troop type
+        if (InfantryToggle.isOn)
+        {
+            InfantryToggleOn.SetActive(true);
+            InfantryToggleOff.SetActive(false);
+        }
+        else if (!InfantryToggle.isOn)
+        {
+            InfantryToggleOn.SetActive(false);
+            InfantryToggleOff.SetActive(true);
+        }
+
+        if (PikeToggle.isOn)
+        {
+            PikeToggleOn.SetActive(true);
+            PikeToggleOff.SetActive(false);
+        }
+        else if (!PikeToggle.isOn)
+        {
+            PikeToggleOn.SetActive(false);
+            PikeToggleOff.SetActive(true);
+        }
+
+        if (ArcherToggle.isOn)
+        {
+            ArcherToggleOn.SetActive(true);
+            ArcherToggleOff.SetActive(false);
+        }
+        else if (!ArcherToggle.isOn)
+        {
+            ArcherToggleOn.SetActive(false);
+            ArcherToggleOff.SetActive(true);
+        }
+
+        if (ScoutToggle.isOn)
+        {
+            ScoutToggleOn.SetActive(true);
+            ScoutToggleOff.SetActive(false);
+        }
+        else if (!ScoutToggle.isOn)
+        {
+            ScoutToggleOn.SetActive(false);
+            ScoutToggleOff.SetActive(true);
+        }
+
+
     }
 }
