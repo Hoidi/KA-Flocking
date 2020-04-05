f1f7afeb4e82b527825a756abe1a4d52084a930f
Max <maxarfvidsson@gmail.com>
2020-04-05 21:34:01 +0200

    made it so that soldiers do not fall over when fighting

--- KA-Flocking/Assets/Scripts/FlockAgent.cs
+++ KA-Flocking/Assets/Scripts/FlockAgent.cs
@@ -16,11 +16,12 @@ public class FlockAgent : MonoBehaviour
     public Collider AgentCollider { get { return agentCollider; } }
     public Animator animator;
     bool attacking = false;
+    Vector3 attackAlignment = Vector3.zero;
     float attackCountDown = 0f;
     public float dragCoefficient = 0.1f;
     int animationMode = 0;
     FightOrFlightBehaviour FOFH;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -43,11 +44,13 @@ public class FlockAgent : MonoBehaviour
             attackCountDown -= Time.deltaTime;
             rb.velocity = Vector3.zero;
             if (attackCountDown <= 0) attacking = false;
+            transform.forward = attackAlignment;
             StabiliseY();
             return;
         }
 
         acceleration.y = 0f;
+        //Debug.Log(acceleration.ToString());
         rb.velocity += acceleration * Time.deltaTime * 10;
         //rb.velocity += HorizontalDrag(rb.velocity); for some reason this breaks everything
 
@@ -59,14 +62,14 @@ public class FlockAgent : MonoBehaviour
         }
 
         float sqrVelocity = rb.velocity.sqrMagnitude;
-        float[] sqrAnimationSpeeds = { 0, 1.3f, 2.5f, 4.5f, 8, 14, 23, 35, 60, 90,};
+        float[] sqrAnimationSpeeds = { 0, 1.3f, 2.5f, 4.5f, 8, 14, 23, 35, 60, 90, };
         int newAnimationMode = animationMode;
 
         //looks to see if the velocity has changed enough to enter a new range. 
         //also ads a buffer of 0.2 and 0.3 respectilvy stopping a unity from jumping between two animations to frequently. 
-        for (int i = sqrAnimationSpeeds.Length-1; i >= 0; i--)
+        for (int i = sqrAnimationSpeeds.Length - 1; i >= 0; i--)
         {
-            if (animationMode > i && sqrAnimationSpeeds[i+1] * 0.8f > sqrVelocity)
+            if (animationMode > i && sqrAnimationSpeeds[i + 1] * 0.8f > sqrVelocity)
             {
                 newAnimationMode = i;
                 break;
@@ -78,7 +81,7 @@ public class FlockAgent : MonoBehaviour
             }
         }
 
-        if(newAnimationMode == animationMode)
+        if (newAnimationMode == animationMode)
         {
             StabiliseY();
             return;
@@ -147,7 +150,7 @@ public class FlockAgent : MonoBehaviour
         Vector3 horizontalSpeed = speed;
         horizontalSpeed.y = 0f;
         return -horizontalSpeed.normalized * horizontalSpeed.sqrMagnitude * 0.1f;
-        
+
     }
 
     public void Attack(List<Transform> targets, FlockAgent attacker, Flock flock)
@@ -163,6 +166,10 @@ public class FlockAgent : MonoBehaviour
                     attackCountDown = 1.4f;
                     ResetAnimations();
                     animator.SetTrigger("meleeAttack");
+
+                    Vector3 velocity = rb.velocity;
+                    velocity.y = 0;
+                    attackAlignment = velocity;  //for making the character still stand up right while fighting. 
                 }
             }
         }
